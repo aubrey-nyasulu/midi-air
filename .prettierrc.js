39190d904e2eafa9d64fc5e8b0bde64f03a51f12
Aubrey Nyasulu <aubreynyasulu98@gmail.com>
2026-02-28 04:12:07 +0200

    initial commit

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+    semi: false, // No semicolons
+    tabWidth: 4, // 4 spaces indentation
+    useTabs: false, // Use spaces, not tabs
+    singleQuote: true, // Use single quotes instead of double
+    trailingComma: 'es5', // Trailing commas where valid in ES5
+    bracketSpacing: true, // Print spaces between brackets in object literals
+    arrowParens: 'avoid', // Omit parens when possible: x => x
+    printWidth: 80, // Line width before wrapping
+}
